9a0edcf02c2dc12f37079161f88fcd8cac4ef57f
young <youngook.kim@gmail.com>
2019-08-07 16:55:55 +0900

    rename the x509 resources

--- x509.tf
+++ x509.tf
@@ -1,12 +1,12 @@
 # spinnaker cli certificate authorities
 
 # kube config
-data "template_file" "cli-x509" {
-  template = file("${path.module}/res/cli-x509.tpl")
+data "template_file" "x509" {
+  template = file("${path.module}/res/x509.tpl")
 }
 
 resource "local_file" "gen-x509-ca" {
-  content  = data.template_file.cli-x509.rendered
+  content  = data.template_file.x509.rendered
   filename = "${path.cwd}/${local.cluster-name}/gen-x509.sh"
 }
 

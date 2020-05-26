3d5997f990333075ffdf8a67de55e2a7a3121578
Young-ook <youngook.kim@gmail.com>
2020-05-26 20:54:49 +0900

    (feat) aws_partition for us-gov, cn regions

--- eks.tf
+++ eks.tf
@@ -25,13 +25,13 @@ data "aws_iam_policy_document" "master-trustrel" {
 
 # eks cluster policy/role 
 resource "aws_iam_role_policy_attachment" "eks-cluster" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  policy_arn = "${format("arn:%s:iam::aws:policy/AmazonEKSClusterPolicy", data.aws_partition.current.partition)}"
   role       = "${aws_iam_role.master.id}"
 }
 
 # eks service policy/role 
 resource "aws_iam_role_policy_attachment" "eks-service" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  policy_arn = "${format("arn:%s:iam::aws:policy/AmazonEKSServicePolicy", data.aws_partition.current.partition)}"
   role       = "${aws_iam_role.master.id}"
 }
 
--- main.tf
+++ main.tf
@@ -1,3 +1,5 @@
 terraform {
   required_version = ">= 0.11.0"
 }
+
+data "aws_partition" "current" {}

5be546c72cbcd1e525c47296432cae6e629628da
Young-ook <youngook.kim@gmail.com>
2020-07-17 20:37:57 +0900

    (feat) added update-halconfig script (#54)
    * (feat) parameterized update-kubeconfig sciprt
    
    * (feat) halyard configuration script
    
    * (fix) simplified and beautified update-x509ca script
    
    * (fix) changed the default path to kubeconfig

---
+++ halyard.tf
@@ -0,0 +1,32 @@
+# x509 certificates for spinnaker cli and helm/tiller communication
+
+# x509 certificates authority
+data "template_file" "update-x509ca" {
+  template = file(format("%s/resources/update-x509ca.tpl", path.module))
+
+  vars = {
+    country      = lookup(var.x509_prop, "country", "KR")
+    state        = lookup(var.x509_prop, "state", "ICN")
+    location     = lookup(var.x509_prop, "location", "ICN")
+    organization = lookup(var.x509_prop, "organization", "ORG")
+    common_name  = lookup(var.x509_prop, "common_name", "your@email.com")
+    groups       = lookup(var.x509_prop, "groups", "admin")
+  }
+}
+
+resource "local_file" "update-x509ca" {
+  content         = data.template_file.update-x509ca.rendered
+  filename        = format("%s/update-x509ca.sh", path.cwd)
+  file_permission = "0500"
+}
+
+# halconfig
+data "template_file" "update-halconfig" {
+  template = file(format("%s/resources/update-halconfig.tpl", path.module))
+}
+
+resource "local_file" "update-halconfig" {
+  content         = data.template_file.update-halconfig.rendered
+  filename        = format("%s/update-halconfig.sh", path.cwd)
+  file_permission = "0500"
+}

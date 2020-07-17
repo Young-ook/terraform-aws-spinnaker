b9a127b95dff627fa8caf97a4b7b6421f110129c
Young-ook <youngook.kim@gmail.com>
2020-07-17 15:58:22 +0900

    (feat) halyard configuration script

--- halyard.tf
+++ halyard.tf
@@ -19,3 +19,14 @@ resource "local_file" "gen-x509-ca" {
   filename        = format("%s/x509.sh", path.cwd)
   file_permission = "0600"
 }
+
+# halconfig
+data "template_file" "update-halconfig" {
+  template = file(format("%s/resources/update-halconfig.tpl", path.module))
+}
+
+resource "local_file" "update-halconfig" {
+  content         = data.template_file.update-halconfig.rendered
+  filename        = format("%s/update-halconfig.sh", path.cwd)
+  file_permission = "0500"
+}

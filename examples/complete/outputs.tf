251778c818be31d32c669f567e718994de2258c3
Young-ook <youngook.kim@gmail.com>
2020-07-21 16:12:57 +0900

    (doc) updated the link to the spinnaker-managed-role module in example (#62)
    * (doc) updated the link to the spinnaker-managed-role module in example
    
    * (fix) updated the sample code to display outputs

---
+++ examples/complete/outputs.tf
@@ -0,0 +1,9 @@
+output "eks_endpoint" {
+  value       = module.spinnaker.endpoint
+  description = "The generated endpoint of eks API server to mamage the cluster from spinnaker module"
+}
+
+output "spinnaker_managed_role_arn" {
+  value       = module.spinnaker-managed-role.role_arn
+  description = "The generated arn from spinnaker managed role module"
+}

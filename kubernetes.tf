7753b8762247ce80254273bf2e35be661805997b
Young-ook <youngook.kim@gmail.com>
2020-07-24 14:41:35 +0900

    (fix) merged kubernetes provider into helm

--- kubernetes.tf
+++ kubernetes.tf
@@ -211,39 +211,28 @@ data "aws_eks_cluster_auth" "eks" {
 
 ### helming!!!
 
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.eks.endpoint
-  token                  = data.aws_eks_cluster_auth.eks.token
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
-  load_config_file       = false
-}
-
-resource "kubernetes_namespace" "spinnaker" {
-  metadata {
-    labels = {
-      app = "spinnaker"
-    }
-    name = "spinnaker"
-  }
-}
-
 provider "helm" {
   kubernetes {
     host                   = data.aws_eks_cluster.eks.endpoint
     token                  = data.aws_eks_cluster_auth.eks.token
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority.0.data)
     load_config_file       = false
   }
 }
 
 resource "helm_release" "spinnaker" {
-  name       = "spinnaker"
-  chart      = "spinnaker"
-  repository = var.helm_repo
-  namespace  = "spinnaker"
-  timeout    = var.helm_timeout
-  version    = var.helm_chart_version
-  values     = var.helm_chart_values
+  name             = "spinnaker"
+  chart            = "spinnaker"
+  namespace        = "spinnaker"
+  repository       = var.helm_repo
+  timeout          = var.helm_timeout
+  version          = var.helm_chart_version
+  values           = var.helm_chart_values
+  create_namespace = true
+  reset_values     = false
 
-  depends_on = [aws_eks_cluster.eks, aws_eks_node_group.ng, kubernetes_namespace.spinnaker]
+  depends_on = [
+    aws_eks_cluster.eks,
+    aws_eks_node_group.ng,
+  ]
 }

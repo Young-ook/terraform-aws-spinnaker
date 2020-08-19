31e00929b41e38da554697a9d979b7b6ea48b90b
Young-ook <youngook.kim@gmail.com>
2020-08-20 00:44:54 +0900

    (fix) removed data variable of eks cluster (#86)

--- main.tf
+++ main.tf
@@ -2,9 +2,9 @@
 
 provider "helm" {
   kubernetes {
-    host                   = data.aws_eks_cluster.eks.endpoint
+    host                   = aws_eks_cluster.eks.endpoint
     token                  = data.aws_eks_cluster_auth.eks.token
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority.0.data)
+    cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority.0.data)
     load_config_file       = false
   }
 }

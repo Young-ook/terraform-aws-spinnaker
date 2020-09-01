d8f2d52385d5b6484f0828b45e33dc5a0b67afd7
Young-ook Kim <youngook.kim@gmail.com>
2020-09-01 17:30:15 +0900

    (fix) added dependency onto the helm release submodules

--- modules/spinnaker-managed-eks/aws-controller.tf
+++ modules/spinnaker-managed-eks/aws-controller.tf
@@ -11,6 +11,7 @@ provider "helm" {
 module "autoscaler" {
   source = "./modules/autoscaler"
 
+  depends_on   = [aws_autoscaling_group.ng]
   providers    = { helm = helm.aws-controller }
   enabled      = (var.node_groups != null ? ((length(var.node_groups) > 0) ? true : false) : false)
   cluster_name = aws_eks_cluster.cp.name
@@ -21,6 +22,7 @@ module "autoscaler" {
 module "albingress" {
   source = "./modules/albingress"
 
+  depends_on   = [aws_autoscaling_group.ng]
   providers    = { helm = helm.aws-controller }
   enabled      = (var.node_groups != null ? ((length(var.node_groups) > 0) ? true : false) : false)
   cluster_name = aws_eks_cluster.cp.name
@@ -31,6 +33,7 @@ module "albingress" {
 module "containerinsights" {
   source = "./modules/containerinsights"
 
+  depends_on   = [aws_autoscaling_group.ng]
   providers    = { helm = helm.aws-controller }
   enabled      = (var.node_groups != null ? ((length(var.node_groups) > 0 && var.container_insights_enabled) ? true : false) : false)
   cluster_name = aws_eks_cluster.cp.name

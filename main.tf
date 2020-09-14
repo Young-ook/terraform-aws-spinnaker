27fb4973ae7ef9d4a6ec5ef08e9a8b77abe0f2e3
Young-ook <youngook.kim@gmail.com>
2020-09-14 12:55:49 +0900

    Spinnaker iam policies reorganization (#103)
    * (feat) reorganized the iam policies for spinnaker
    
    * (feat) updated spinnaker example

--- main.tf
+++ main.tf
@@ -29,6 +29,99 @@ module "rds" {
   aurora_instances = var.aurora_instances
 }
 
+# security/policy
+resource "aws_iam_policy" "rosco-bake" {
+  name = format("%s-bake", local.name)
+  policy = jsonencode({
+    Statement = [{
+      Action = [
+        "iam:PassRole",
+        "ec2:AttachVolume",
+        "ec2:AuthorizeSecurityGroupIngress",
+        "ec2:CopyImage",
+        "ec2:CreateImage",
+        "ec2:CreateKeypair",
+        "ec2:CreateSecurityGroup",
+        "ec2:CreateSnapshot",
+        "ec2:CreateTags",
+        "ec2:CreateVolume",
+        "ec2:DeleteKeyPair",
+        "ec2:DeleteSecurityGroup",
+        "ec2:DeleteSnapshot",
+        "ec2:DeleteVolume",
+        "ec2:DeregisterImage",
+        "ec2:DescribeImageAttribute",
+        "ec2:DescribeImages",
+        "ec2:DescribeInstances",
+        "ec2:DescribeRegions",
+        "ec2:DescribeSecurityGroups",
+        "ec2:DescribeSnapshots",
+        "ec2:DescribeSubnets",
+        "ec2:DescribeTags",
+        "ec2:DescribeVolumes",
+        "ec2:DetachVolume",
+        "ec2:GetPasswordData",
+        "ec2:ModifyImageAttribute",
+        "ec2:ModifyInstanceAttribute",
+        "ec2:ModifySnapshotAttribute",
+        "ec2:RegisterImage",
+        "ec2:RunInstances",
+        "ec2:StopInstances",
+        "ec2:TerminateInstances",
+        "ec2:RequestSpotInstances",
+        "ec2:CancelSpotInstanceRequests",
+        "ec2:DescribeSpotInstanceRequests",
+        "ec2:DescribeSpotPriceHistory",
+      ]
+      Effect   = "Allow"
+      Resource = ["*"]
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "rosco-bake" {
+  policy_arn = aws_iam_policy.rosco-bake.arn
+  role       = module.eks.role.name
+}
+
+resource "aws_iam_policy" "ec2-read" {
+  name = format("%s-ec2-read", local.name)
+  policy = jsonencode({
+    Statement = [{
+      Action   = "ec2:Describe*"
+      Effect   = "Allow"
+      Resource = ["*"]
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ec2-read" {
+  policy_arn = aws_iam_policy.ec2-read.arn
+  role       = module.eks.role.name
+}
+
+###
+# assume to cross account spinnaker-managed role
+###
+resource "aws_iam_policy" "spin-assume" {
+  name = format("%s-assume", local.name)
+  policy = jsonencode({
+    Statement = [{
+      Action   = "sts:AssumeRole"
+      Effect   = "Allow"
+      Resource = var.assume_role_arn
+    }]
+    Version = "2012-10-17"
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "spin-assume" {
+  policy_arn = aws_iam_policy.spin-assume.arn
+  role       = module.eks.role.name
+}
+
 ### helming!!!
 
 provider "helm" {

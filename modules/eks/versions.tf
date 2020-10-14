678edec5d32bca1c01a1aaf6f953b0a97ff4bd46
Young-ook <youngook.kim@gmail.com>
2020-10-14 13:15:06 +0900

    Removed the required providers definition (#114)
    * (chore) removed the required providers definition
    
    * (doc) README update

--- modules/eks/versions.tf
+++ modules/eks/versions.tf
@@ -2,18 +2,4 @@
 
 terraform {
   required_version = ">= 0.13"
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-    }
-    helm = {
-      source = "hashicorp/helm"
-    }
-    kubernetes = {
-      source = "hashicorp/kubernetes"
-    }
-    template = {
-      source = "hashicorp/template"
-    }
-  }
 }
--- modules/spinnaker-managed-ecs/versions.tf
+++ modules/spinnaker-managed-ecs/versions.tf
@@ -2,12 +2,4 @@
 
 terraform {
   required_version = ">= 0.13"
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-    }
-    template = {
-      source = "hashicorp/template"
-    }
-  }
 }

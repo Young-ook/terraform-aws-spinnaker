2f86c9e2f8f66d68c396df1f756ee1dd34b2f102
Young-ook Kim <youngook.kim@gmail.com>
2020-11-28 22:38:55 +0900

    feat: spinnaker example test case update

--- examples/spinnaker/main.tf
+++ examples/spinnaker/main.tf
@@ -15,37 +15,19 @@ provider "aws" {
 
 # spinnaker
 module "spinnaker" {
-  source  = "Young-ook/spinnaker/aws"
-  version = "~> 2.0"
-
-  name   = var.name
-  stack  = var.stack
-  detail = var.detail
-  tags   = var.tags
-  region = "ap-northeast-2"
-  azs    = ["ap-northeast-2a", "ap-northeast-2b", "ap-northeast-2c"]
-  cidr   = "10.0.0.0/16"
-
-  kubernetes_version = "1.17"
-  kubernetes_node_groups = [
-    {
-      name          = "default"
-      instance_type = "m5.large"
-      min_size      = "1"
-      max_size      = "3"
-      desired_size  = "2"
-    }
-  ]
-
-  aurora_cluster = {
-    version = "5.7.12"
-    port    = "3306"
-  }
-  aurora_instances = {
-    main = {
-      node_type = "db.t3.medium"
-    }
-  }
+  source                 = "Young-ook/spinnaker/aws"
+  version                = "~> 2.0"
+  name                   = var.name
+  stack                  = var.stack
+  detail                 = var.detail
+  tags                   = var.tags
+  region                 = var.aws_region
+  azs                    = var.azs
+  cidr                   = var.cidr
+  kubernetes_version     = var.kubernetes_version
+  kubernetes_node_groups = var.kubernetes_node_groups
+  aurora_cluster         = var.aurora_cluster
+  aurora_instances       = var.aurora_instances
 
   helm = {
     version = "2.2.2"
@@ -56,9 +38,8 @@ module "spinnaker" {
 
 # spinnaker managed role
 module "spinnaker-managed-role" {
-  source  = "Young-ook/spinnaker/aws//modules/spinnaker-managed-aws"
-  version = "~> 2.0"
-
+  source           = "Young-ook/spinnaker/aws//modules/spinnaker-managed-aws"
+  version          = "~> 2.0"
   providers        = { aws = aws.prod }
   name             = "example"
   stack            = "dev"

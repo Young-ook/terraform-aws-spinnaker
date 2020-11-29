8ee3350815fb57520d233a9cfdb6e8a7c2025842
Young-ook Kim <youngook.kim@gmail.com>
2020-11-29 15:56:23 +0900

    feat: add features output variable to the fargate example

--- examples/spinnaker-managed-ecs/outputs.tf
+++ examples/spinnaker-managed-ecs/outputs.tf
@@ -12,3 +12,8 @@ output "ecs-fargate" {
   description = "The generated AWS ECS cluster"
   value       = module.spinnaker-managed-ecs-fargate.cluster
 }
+
+output "ecs-fargate-features" {
+  description = "Features configurations of the AWS Fargate cluster"
+  value       = module.spinnaker-managed-ecs-fargate.features
+}

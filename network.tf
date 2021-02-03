007282a8dddcd0ead3636412f8b0495926ea98bc
Young-ook Kim <youngook.kim@gmail.com>
2021-02-04 00:21:26 +0900

    fix(spinnaker): specified the type of vpc endpoint for s3

--- network.tf
+++ network.tf
@@ -120,7 +120,8 @@ resource "aws_route_table_association" "public" {
 
 # vpc endpoints
 data "aws_vpc_endpoint_service" "s3" {
-  service = "s3"
+  service      = "s3"
+  service_type = "Gateway"
 }
 
 resource "aws_vpc_endpoint" "s3" {

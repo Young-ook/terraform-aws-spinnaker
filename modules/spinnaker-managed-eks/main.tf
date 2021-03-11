5d609ffa1a91646eea5f47c4b84d6cc71af0db8b
Young-ook Kim <youngook.kim@gmail.com>
2021-03-11 14:52:38 +0900

    feat(eks): eks module and kubernetes examepl version upgrade

--- modules/spinnaker-managed-eks/main.tf
+++ modules/spinnaker-managed-eks/main.tf
@@ -1,6 +1,6 @@
 module "eks" {
   source                    = "Young-ook/eks/aws"
-  version                   = "1.4.3"
+  version                   = "1.4.7"
   name                      = local.name
   tags                      = var.tags
   subnets                   = var.subnets

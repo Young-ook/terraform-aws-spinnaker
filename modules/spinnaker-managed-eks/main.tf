36293fe499267ae91e92dfbb5a4fe9fc0c876c97
Young-ook <youngook.kim@gmail.com>
2021-04-04 23:27:47 +0900

    feat(eks): module upgrade - supports additional policies for node groups (#152)

--- modules/spinnaker-managed-eks/main.tf
+++ modules/spinnaker-managed-eks/main.tf
@@ -1,6 +1,6 @@
 module "eks" {
   source                    = "Young-ook/eks/aws"
-  version                   = "1.4.7"
+  version                   = "1.4.8"
   name                      = local.name
   tags                      = var.tags
   subnets                   = var.subnets
@@ -8,4 +8,5 @@ module "eks" {
   enabled_cluster_log_types = var.enabled_cluster_log_types
   node_groups               = var.node_groups
   managed_node_groups       = var.managed_node_groups
+  policy_arns               = var.policy_arns
 }

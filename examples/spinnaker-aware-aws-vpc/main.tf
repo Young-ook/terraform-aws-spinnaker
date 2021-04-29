690bd327d118f210f8d6200f10e0325bea1907b7
Young-ook <youngook.kim@gmail.com>
2021-04-29 16:24:56 +0900

    feat(vpc): support isolated vpc option (#155)

--- examples/spinnaker-aware-aws-vpc/main.tf
+++ examples/spinnaker-aware-aws-vpc/main.tf
@@ -3,9 +3,8 @@ terraform {
 }
 
 provider "aws" {
-  region              = var.aws_region
-  allowed_account_ids = [var.aws_account_id]
-  version             = ">= 3.0"
+  region  = var.aws_region
+  version = ">= 3.0"
 }
 
 # spinnaker aware amazon vpc
@@ -18,6 +17,7 @@ module "spinnaker-aware-aws-vpc" {
   tags       = var.tags
   azs        = var.azs
   cidr       = var.cidr
+  enable_igw = var.enable_igw
   enable_ngw = var.enable_ngw
   single_ngw = var.single_ngw
 }
--- examples/spinnaker-aware-aws-vpc/tc3.tfvars
+++ examples/spinnaker-aware-aws-vpc/tc3.tfvars
@@ -10,5 +10,6 @@ tags = {
 aws_region = "ap-northeast-2"
 azs        = ["ap-northeast-2a", "ap-northeast-2b", "ap-northeast-2c"]
 cidr       = "10.1.0.0/16"
+enable_igw = true
 enable_ngw = true
 single_ngw = true

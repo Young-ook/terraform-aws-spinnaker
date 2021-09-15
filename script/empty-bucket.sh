996a33b841871f7bc86d8c0b2dbcafb97be0b836
Young-ook <youngook.kim@gmail.com>
2021-09-15 18:53:56 +0900

    feat(script): empty s3 bucket script (#172)

---
+++ script/empty-bucket.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# delete all object in a specific S3 bucket
+
+CURDIR=`dirname $0`
+
+function print_usage() {
+  echo "Usage: $0 -b(bucket) <bucket-name> -r(region) <aws-region>"
+}
+
+function process_args() {
+  if [[ $# < 1 ]]; then
+    print_usage
+    exit -1
+  fi
+
+  while getopts ":n:a:r:k:s:" opt; do
+    case $opt in
+      b) BUCKET_NAME="$OPTARG"
+      ;;
+      r) AWS_REGION="$OPTARG"
+      ;;
+      \?)
+        >&2 echo "Unrecognized argument '$OPTARG'"
+      ;;
+    esac
+  done
+}
+
+function empty() {
+  aws s3api delete-objects \
+    --region $AWS_REGION \
+    --bucket $BUCKET_NAME \
+    --delete "$(aws s3api list-object-versions \
+      --region $AWS_REGION \
+      --bucket $BUCKET_NAME \
+      --query='{Objects: Versions[].{Key:Key,VersionId:VersionId}}' \
+      --output json)"
+}
+
+# main
+process_args "$@"
+empty

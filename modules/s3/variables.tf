34af0877eca32a0d2280cacee096bc96daac85a2
Young-ook Kim <youngook.kim@gmail.com>
2021-09-20 22:42:05 +0900

    feat(s3): empty bucket script

--- modules/s3/variables.tf
+++ modules/s3/variables.tf
@@ -47,7 +47,7 @@ variable "versioning" {
 }
 
 variable "force_destroy" {
-  description = "A boolean that indicates all objects should be deleted from the bucket so that the bucket can be destroyed without error."
+  description = "A boolean that indicates all objects should be deleted from the bucket so that the bucket can be destroyed without error"
   type        = bool
   default     = false
 }

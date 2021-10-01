6a1408d640e900f0ada1449338b64b4841005555
Young-ook Kim <youngook.kim@gmail.com>
2021-10-01 18:09:00 +0900

    fix(spinnaker): module outputs grooming

--- examples/spinnaker/outputs.tf
+++ examples/spinnaker/outputs.tf
@@ -1,24 +1,12 @@
-output "eks_endpoint" {
-  description = "The generated endpoint of eks API server to mamage the cluster from spinnaker module"
-  value       = module.spinnaker.eks.endpoint
-}
-
-output "spinnaker_role_arn" {
-  description = "The generated role ARN of eks node group from spinnaker module"
-  value       = module.spinnaker.role.arn
+output "spinnaker" {
+  description = "Attributes of generated spinnaker"
+  value = {
+    artifact_bucket = module.artifact.bucket.id
+    kubeconfig      = module.spinnaker.kubeconfig
+  }
 }
 
 output "spinnaker_managed_role_arn" {
-  value       = module.spinnaker-managed-role.role_arn
   description = "The generated ARN from spinnaker managed role module"
-}
-
-output "artifact" {
-  description = "Attributes of generated bucket for artifacts"
-  value       = module.artifact.bucket
-}
-
-output "kubeconfig" {
-  description = "Bash script to update the kubeconfig file for the EKS cluster"
-  value       = module.spinnaker.kubeconfig
+  value       = module.spinnaker-managed-role.role_arn
 }

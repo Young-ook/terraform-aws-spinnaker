f1fad9a530bf66baf2cd5b4f4d653ca954c7f47e
Young-ook <youngook.kim@gmail.com>
2021-10-25 17:47:02 +0900

    feat(codebuild): cloudwatch log group example (#188)

--- examples/codebuild/outputs.tf
+++ examples/codebuild/outputs.tf
@@ -2,3 +2,8 @@ output "project" {
   description = "The CodeBuild project attributes"
   value       = module.ci.project
 }
+
+output "log" {
+  description = "Attributes of cloudwatch log group"
+  value       = module.logs
+}

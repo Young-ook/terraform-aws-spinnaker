d1650ebf4edc4a098a51d03ac925ed64b73c3040
Young-ook <youngook.kim@gmail.com>
2021-11-03 15:57:56 +0900

    feat(codebuild): start build cli (#191)

--- examples/codebuild/outputs.tf
+++ examples/codebuild/outputs.tf
@@ -3,6 +3,11 @@ output "project" {
   value       = module.ci.project
 }
 
+output "build" {
+  description = "AWS CLI command to start build project"
+  value       = module.ci.build
+}
+
 output "log" {
   description = "Attributes of cloudwatch log group"
   value       = module.logs
--- modules/codebuild/outputs.tf
+++ modules/codebuild/outputs.tf
@@ -4,3 +4,13 @@ output "project" {
   description = "The CodeBuild project attributes"
   value       = aws_codebuild_project.cb
 }
+
+output "build" {
+  description = "AWS CLI command to start build project"
+  value = join(" ", [
+    "aws codebuild start-build",
+    "--region ${data.aws_region.current.name}",
+    "--output text",
+    "--project-name ${aws_codebuild_project.cb.id}",
+  ])
+}

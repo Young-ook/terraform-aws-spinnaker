22ee0f85fde34312b31c73f6544ae7d1a1e290b4
youngookkim <youngook.kim@gmail.com>
2022-03-05 14:09:52 +0900

    feat(frigga): random name (#196)

--- modules/frigga/main.tf
+++ modules/frigga/main.tf
@@ -8,8 +8,14 @@ resource "random_string" "suffix" {
   special = false
 }
 
+resource "random_pet" "name" {
+  length    = 3
+  separator = "-"
+}
+
 locals {
-  suffix   = var.petname ? random_string.suffix.result : ""
-  name     = join("-", compact([var.name, var.stack, var.detail, local.suffix]))
-  name-tag = { "Name" = local.name }
+  suffix      = var.petname ? random_string.suffix.result : ""
+  name        = var.name == null || var.name == "" ? random_pet.name.id : var.name
+  frigga-name = join("-", compact([local.name, var.stack, var.detail, local.suffix]))
+  name-tag    = { "Name" = local.frigga-name }
 }
--- modules/frigga/variables.tf
+++ modules/frigga/variables.tf
@@ -2,18 +2,31 @@
 variable "name" {
   description = "The logical name of the application/service instance"
   type        = string
+  default     = null
+  validation {
+    condition     = var.name == null ? true : var.name != null && length(var.name) > 0
+    error_message = "Length of name is longer than 0."
+  }
 }
 
 variable "stack" {
   description = "Text used to identify stack of infrastructure components (e.g., dev, prod)"
   type        = string
   default     = ""
+  validation {
+    condition     = var.stack != null
+    error_message = "Stak must not be null."
+  }
 }
 
 variable "detail" {
   description = "The purpose or extra description of your application/service instance"
   type        = string
   default     = ""
+  validation {
+    condition     = var.detail != null
+    error_message = "Detail must not be null."
+  }
 }
 
 variable "petname" {

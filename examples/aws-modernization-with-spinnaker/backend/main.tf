e6369362b772ae73297235a0dcbc042a1629a05f
youngookkim <youngook.kim@gmail.com>
2022-06-30 11:29:54 +0900

    fix(devops): enable force destroy feature for terraform backend (#229)

--- examples/aws-modernization-with-spinnaker/backend/main.tf
+++ examples/aws-modernization-with-spinnaker/backend/main.tf
@@ -1,8 +1,9 @@
 ### terraform remote state backend
 
 module "tfstate" {
-  source  = "Young-ook/tfstate-backend/aws"
-  version = "1.0.3"
-  name    = var.name
-  tags    = var.tags
+  source        = "Young-ook/tfstate-backend/aws"
+  version       = "1.0.3"
+  name          = var.name
+  tags          = var.tags
+  force_destroy = true
 }

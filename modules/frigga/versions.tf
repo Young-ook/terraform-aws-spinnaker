5714ce2a2718263377e92d1577475a3043833f14
Young-ook Kim <youngook.kim@gmail.com>
2022-09-10 20:37:37 +0900

    chore(frigga): update required providers

--- modules/frigga/versions.tf
+++ modules/frigga/versions.tf
@@ -3,9 +3,17 @@
 terraform {
   required_version = ">= 0.12"
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 4.0"
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 3.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = ">= 2.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.0"
     }
   }
 }

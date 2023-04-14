0633ec1c56dee824a995f67940815566416e4158
Young-ook Kim <youngook.kim@gmail.com>
2023-04-14 22:16:09 +0900

    fix(modernapp): replace eks module

--- examples/aws-modernization-with-spinnaker/foundation/main.tf
+++ examples/aws-modernization-with-spinnaker/foundation/main.tf
@@ -1,3 +1,8 @@
+### aws partitions
+module "aws" {
+  source = "Young-ook/spinnaker/aws//modules/aws-partitions"
+}
+
 ### foundation/network
 module "vpc" {
   source     = "Young-ook/spinnaker/aws//modules/spinnaker-aware-aws-vpc"
@@ -12,8 +17,8 @@ module "vpc" {
 
 ### foundation/kubernetes
 module "eks" {
-  source             = "Young-ook/spinnaker/aws//modules/spinnaker-managed-eks"
-  version            = "2.1.15"
+  source             = "Young-ook/eks/aws"
+  version            = "2.0.3"
   name               = var.name
   tags               = merge(var.tags, { release = "canary" })
   subnets            = values(module.vpc.subnets["private"])
@@ -29,54 +34,113 @@ module "eks" {
     }
   ]
   policy_arns = [
-    "arn:aws:iam::aws:policy/AWSXRayDaemonWriteAccess",
-    "arn:aws:iam::aws:policy/AWSAppMeshEnvoyAccess",
+    format("arn:%s:iam::aws:policy/AWSXRayDaemonWriteAccess", module.aws.partition.partition),
+    format("arn:%s:iam::aws:policy/AWSAppMeshEnvoyAccess", module.aws.partition.partition),
   ]
 }
 
 provider "helm" {
   kubernetes {
-    host                   = module.eks.helmconfig.host
-    token                  = module.eks.helmconfig.token
-    cluster_ca_certificate = base64decode(module.eks.helmconfig.ca)
+    host                   = module.eks.kubeauth.host
+    token                  = module.eks.kubeauth.token
+    cluster_ca_certificate = module.eks.kubeauth.ca
   }
 }
 
-module "lb-controller" {
-  source       = "Young-ook/eks/aws//modules/lb-controller"
-  version      = "1.7.5"
-  cluster_name = module.eks.cluster.name
-  oidc         = module.eks.oidc
-  tags         = var.tags
+### helm-addons
+module "helm-addons" {
+  depends_on = [module.eks]
+  source     = "Young-ook/eks/aws//modules/helm-addons"
+  version    = "2.0.0"
+  tags       = var.tags
+  addons = [
+    {
+      repository     = "https://aws.github.io/eks-charts"
+      name           = "appmesh-controller"
+      chart_name     = "appmesh-controller"
+      namespace      = "kube-system"
+      serviceaccount = "appmesh-controller"
+      values = {
+        "region"           = module.aws.region.name
+        "tracing.enabled"  = true
+        "tracing.provider" = "x-ray"
+      }
+      oidc = module.eks.oidc
+      policy_arns = [
+        format("arn:%s:iam::aws:policy/AWSAppMeshEnvoyAccess", module.aws.partition.partition),
+        format("arn:%s:iam::aws:policy/AWSCloudMapFullAccess", module.aws.partition.partition),
+        format("arn:%s:iam::aws:policy/AWSXRayDaemonWriteAccess", module.aws.partition.partition),
+      ]
+    },
+    {
+      repository     = "https://aws.github.io/eks-charts"
+      name           = "aws-cloudwatch-metrics"
+      chart_name     = "aws-cloudwatch-metrics"
+      namespace      = "kube-system"
+      serviceaccount = "aws-cloudwatch-metrics"
+      values = {
+        "clusterName" = module.eks.cluster.name
+      }
+      oidc = module.eks.oidc
+      policy_arns = [
+        format("arn:%s:iam::aws:policy/CloudWatchAgentServerPolicy", module.aws.partition.partition)
+      ]
+    },
+    {
+      repository     = "https://aws.github.io/eks-charts"
+      name           = "aws-for-fluent-bit"
+      chart_name     = "aws-for-fluent-bit"
+      namespace      = "kube-system"
+      serviceaccount = "aws-for-fluent-bit"
+      values = {
+        "cloudWatch.enabled"      = true
+        "cloudWatch.region"       = module.aws.region.name
+        "cloudWatch.logGroupName" = format("/aws/containerinsights/%s/application", module.eks.cluster.name)
+        "firehose.enabled"        = false
+        "kinesis.enabled"         = false
+        "elasticsearch.enabled"   = false
+      }
+      oidc = module.eks.oidc
+      policy_arns = [
+        format("arn:%s:iam::aws:policy/CloudWatchAgentServerPolicy", module.aws.partition.partition)
+      ]
+    },
+    {
+      repository     = "https://aws.github.io/eks-charts"
+      name           = "aws-load-balancer-controller"
+      chart_name     = "aws-load-balancer-controller"
+      namespace      = "kube-system"
+      serviceaccount = "aws-load-balancer-controller"
+      values         = { "clusterName" = module.eks.cluster.name }
+      oidc           = module.eks.oidc
+      policy_arns    = [aws_iam_policy.lbc.arn]
+    },
+    {
+      repository     = "${path.module}/charts/"
+      name           = "cluster-autoscaler"
+      chart_name     = "cluster-autoscaler"
+      namespace      = "kube-system"
+      serviceaccount = "cluster-autoscaler"
+      values = {
+        "awsRegion"                 = module.aws.region.name
+        "autoDiscovery.clusterName" = module.eks.cluster.name
+      }
+      oidc        = module.eks.oidc
+      policy_arns = [aws_iam_policy.cas.arn]
+    },
+  ]
 }
 
-module "app-mesh" {
-  source       = "Young-ook/eks/aws//modules/app-mesh"
-  version      = "1.7.5"
-  cluster_name = module.eks.cluster.name
-  oidc         = module.eks.oidc
-  tags         = var.tags
-  helm = {
-    version = "1.3.0"
-  }
+resource "aws_iam_policy" "lbc" {
+  name        = "aws-loadbalancer-controller"
+  tags        = merge({ "terraform.io" = "managed" }, var.tags)
+  description = format("Allow aws-load-balancer-controller to manage AWS resources")
+  policy      = file("${path.module}/policy.aws-loadbalancer-controller.json")
 }
 
-module "container-insights" {
-  source       = "Young-ook/eks/aws//modules/container-insights"
-  version      = "1.7.5"
-  cluster_name = module.eks.cluster.name
-  oidc         = module.eks.oidc
-  tags         = var.tags
-  features = {
-    enable_metrics = true
-    enable_logs    = true
-  }
-}
-
-module "cluster-autoscaler" {
-  source       = "Young-ook/eks/aws//modules/cluster-autoscaler"
-  version      = "1.7.5"
-  cluster_name = module.eks.cluster.name
-  oidc         = module.eks.oidc
-  tags         = var.tags
+resource "aws_iam_policy" "cas" {
+  name        = "cluster-autoscaler"
+  tags        = merge({ "terraform.io" = "managed" }, var.tags)
+  description = format("Allow cluster-autoscaler to manage AWS resources")
+  policy      = file("${path.module}/policy.cluster-autoscaler.json")
 }

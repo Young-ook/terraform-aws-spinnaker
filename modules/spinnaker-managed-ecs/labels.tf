b3f020d49a1a2b679393c3ed37c493db13695bc5
Young-ook Kim <youngook.kim@gmail.com>
2023-06-27 18:41:47 +0900

    fix(spinnaker-ecs): frigga name module version

--- modules/spinnaker-managed-ecs/labels.tf
+++ modules/spinnaker-managed-ecs/labels.tf
@@ -7,16 +7,18 @@ data "aws_caller_identity" "current" {}
 
 # frigga naming
 module "frigga" {
-  source = "Young-ook/spinnaker/aws//modules/frigga"
-  name   = var.name
-  stack  = var.stack
-  detail = var.detail
+  source  = "Young-ook/spinnaker/aws//modules/frigga"
+  version = "2.3.5"
+  name    = var.name
+  stack   = var.stack
+  detail  = var.detail
 }
 
 locals {
   name = module.frigga.name
   default-tags = merge(
     { "terraform.io" = "managed" },
+    { "spinnaker.io" = "managed" },
     local.ecs-tag
   )
 }

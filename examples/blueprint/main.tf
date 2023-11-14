e7fae02cf54e74e441e8479e665c574174fc92b5
Young-ook Kim <youngook.kim@gmail.com>
2023-11-14 20:19:27 +0900

    fix: upgrade spinnaker module to 3.0.0

--- examples/blueprint/main.tf
+++ examples/blueprint/main.tf
@@ -7,7 +7,7 @@ provider "aws" {
 ### network
 module "spinnaker-aware-aws-vpc" {
   source     = "Young-ook/spinnaker/aws//modules/spinnaker-aware-aws-vpc"
-  version    = "2.3.6"
+  version    = "3.0.0"
   name       = var.name
   stack      = "preprod"
   tags       = var.tags
@@ -20,58 +20,21 @@ module "spinnaker-aware-aws-vpc" {
 
 ### platform/spinnaker
 module "spinnaker" {
-  source                 = "Young-ook/spinnaker/aws"
-  version                = "2.3.6"
-  name                   = var.name
-  stack                  = "preprod"
-  tags                   = var.tags
-  vpc                    = module.spinnaker-aware-aws-vpc.vpc.id
-  subnets                = values(module.spinnaker-aware-aws-vpc.subnets["private"])
-  cidrs                  = [module.spinnaker-aware-aws-vpc.vpc.cidr_block]
-  assume_role_arn        = [module.spinnaker-managed.role_arn]
-  kubernetes_version     = var.kubernetes_version
-  kubernetes_policy_arns = [module.artifact.policy_arns["read"]]
-  kubernetes_node_groups = [
-    {
-      name          = "cd"
-      instance_type = "m5.xlarge"
-      min_size      = "1"
-      max_size      = "3"
-      desired_size  = "1"
+  source  = "Young-ook/spinnaker/aws"
+  version = "3.0.0"
+  name    = var.name
+  tags    = var.tags
+  features = {
+    #    aurora = { enabled = true }
+    #    s3 = { enabled = true }
+    eks = {
+      ssm_enabled = true
+      version     = var.kubernetes_version
     }
-  ]
-  aurora_cluster = {
-    engine            = "aurora-mysql"
-    version           = "5.7.mysql_aurora.2.07.2"
-    port              = "3306"
-    backup_retention  = "1"
-    apply_immediately = "true"
-    cluster_parameters = {
-      character_set_server = "utf8"
-      character_set_client = "utf8"
+    vpc = {
+      id      = module.spinnaker-aware-aws-vpc.vpc.id
+      subnets = values(module.spinnaker-aware-aws-vpc.subnets["private"])
+      cidrs   = [module.spinnaker-aware-aws-vpc.vpc.cidr_block]
     }
   }
-  aurora_instances = [
-    {
-      instance_type = "db.t3.medium"
-    }
-  ]
-  s3_bucket = {
-    force_destroy = true
-  }
-  helm = {
-    vars = {
-      "halyard.spinnakerVersion" = "1.27.0"
-      "halyard.image.tag"        = "1.44.0"
-    }
-  }
-}
-
-### environment/preprod
-module "spinnaker-managed" {
-  source           = "Young-ook/spinnaker/aws//modules/spinnaker-managed-aws"
-  version          = "2.3.6"
-  name             = "devops"
-  stack            = "preprod"
-  trusted_role_arn = [module.spinnaker.role.arn]
 }

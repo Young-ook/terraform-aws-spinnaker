6187aaa5ec62074a097f805d17576288bc77a573
Young-ook Kim <youngook.kim@gmail.com>
2024-01-16 23:00:32 +0900

    chore(codebuild): change the scripts dir name

--- modules/codebuild/outputs.tf
+++ modules/codebuild/outputs.tf
@@ -9,7 +9,7 @@ output "build" {
   description = "Bash script to start a build proejct"
   value = join(" ", [
     "bash -e",
-    format("%s/script/start-build.sh", path.module),
+    format("%s/scripts/start-build.sh", path.module),
     format("-r %s", module.aws.region.name),
     format("-n %s", aws_codebuild_project.cb.id),
   ])
